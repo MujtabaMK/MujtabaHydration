fb6465389c048ceb0e77e4802882d8e3bfca136a
MujtabaMK <mujtabakhanakola@gmail.com>
2024-03-27 12:27:42 +0530

    Initial Commit

---
+++ MujtabaHydration/MujtabaHydrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MujtabaHydrationApp.swift
+//  MujtabaHydration
+//
+//  Created by Mujtaba Khan on 27/03/24.
+//
+
+import SwiftUI
+
+@main
+struct MujtabaHydrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
