5c88000f3b43d3081ea4ca56c4fc47ed28cf63f9
MujtabaMK <mujtabakhanakola@gmail.com>
2024-03-27 12:50:26 +0530

    Create Helper Function

---
+++ MujtabaHydration/Helpers/CustomButtons.swift
@@ -0,0 +1,35 @@
+//
+//  CustomButtons.swift
+//  MujtabaHydration
+//
+//  Created by Mujtaba Khan on 27/03/24.
+//
+
+import Foundation
+import SwiftUI
+
+
+// Blue fade
+struct customButtonStyle: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .foregroundColor(.white)
+            .padding()
+            .background(LinearGradient(gradient: Gradient(colors: [Color.blue, Color.blue.opacity(0.6)]), startPoint: /*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/, endPoint: /*@START_MENU_TOKEN@*/.trailing/*@END_MENU_TOKEN@*/))
+            .cornerRadius(10)
+            .scaleEffect(configuration.isPressed ? 1.02 : 1.0)
+    }
+}
+
+
+// Orange fade
+struct secondaryButtonStyle: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .foregroundColor(.white)
+            .padding()
+            .background(LinearGradient(gradient: Gradient(colors: [Color.orange, Color.orange.opacity(0.6)]), startPoint: /*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/, endPoint: /*@START_MENU_TOKEN@*/.trailing/*@END_MENU_TOKEN@*/))
+            .cornerRadius(10)
+            .scaleEffect(configuration.isPressed ? 1.02 : 1.0)
+    }
+}
